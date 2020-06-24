beb181c3ef2ca1a3260dce2144f68df7f333303c
Roman <roman.yavorskyi@hotmail.com>
2020-06-24 16:10:01 +0300

    Add project files.

---
+++ NotificationTest/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DesktopNotifications;
+
+namespace NotificationTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Register AUMID and COM server (for MSIX/sparse package apps, this no-ops)
+            DesktopNotificationManagerCompat.RegisterAumidAndComServer<MyNotificationActivator>("Notification.Test123");
+            // Register COM server and activator type
+            DesktopNotificationManagerCompat.RegisterActivator<MyNotificationActivator>();
+            base.OnStartup(e);
+            var window = new MainWindow();
+            window.Show();
+        }
+    }
+}
---
+++ NotificationTest/MyNotificationActivator.cs
@@ -0,0 +1,17 @@
+﻿// The GUID CLSID must be unique to your app. Create a new GUID if copying this code.
+
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Threading;
+using DesktopNotifications;
+
+[ClassInterface(ClassInterfaceType.None)]
+[ComSourceInterfaces(typeof(NotificationActivator.INotificationActivationCallback))]
+[Guid("b36a1f69-9a84-4a35-a3ee-bfd796c14256"), ComVisible(true)]
+public class MyNotificationActivator : NotificationActivator
+{
+    public override void OnActivated(string invokedArgs, NotificationUserInput userInput, string appUserModelId)
+    {
+        Dispatcher.CurrentDispatcher.Invoke(() => MessageBox.Show("Notification captured"));
+    }
+}
